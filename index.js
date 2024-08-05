309bdf107c86fbd466abb135ffa73afe5bc56d02
FafaelDuarte <155685529+Rafafrias2012@users.noreply.github.com>
2024-08-05 11:34:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,72 +3,42 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+let users = {};
+
 app.use(express.static('public'));
 
-let users = {};
-let members = {};
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 io.on('connection', (socket) => {
-  console.log('New connection');
+  console.log('a user connected');
 
-  socket.on('join', (data) => {
-    let user = {
-      id: socket.id,
-      nickname: data.nickname,
-      roomId: data.roomId,
-      isAdmin: false
-    };
-    users[socket.id] = user;
-    socket.emit('join', user);
-    socket.broadcast.emit('join', user);
+  socket.on('join', ({ user, room }) => {
+    users[user.id] = user;
+    socket.join(room);
+    io.in(room).emit('join', user);
   });
 
-  socket.on('talk', (text) => {
-    if (members[socket.id]) {
-      socket.emit('talk', { id: socket.id, text: text });
-      socket.broadcast.emit('talk', { id: socket.id, text: text });
-    }
+  socket.on('talk', ({ user, text }) => {
+    io.in(user.room).emit('talk', { user, text });
   });
 
-  socket.on('update', (data) => {
-    users[socket.id].nickname = data.nickname;
-    users[socket.id].isAdmin = data.isAdmin;
-    socket.emit('update', users[socket.id]);
-    socket.broadcast.emit('update', users[socket.id]);
+  socket.on('updateUser', (user) => {
+    users[user.id] = user;
+    io.in(user.room).emit('update', user);
   });
 
-  socket.on('kick', (id) => {
-    if (users[id]) {
-      socket.emit('leave', id);
-      socket.broadcast.emit('leave', id);
+  socket.on('kickUser', (id) => {
+    const user = users[id];
+    if (user) {
+      io.in(user.room).emit('leave', id);
       delete users[id];
-      delete members[id];
-    }
-  });
-
-  socket.on('mute', (id) => {
-    if (users[id]) {
-      members[id] = false;
-      socket.emit('mute', id);
-      socket.broadcast.emit('mute', id);
-    }
-  });
-
-  socket.on('unmute', (id) => {
-    if (users[id]) {
-      members[id] = true;
-      socket.emit('unmute', id);
-      socket.broadcast.emit('unmute', id);
     }
   });
 
   socket.on('disconnect', () => {
-    if (users[socket.id]) {
-      socket.emit('leave', socket.id);
-      socket.broadcast.emit('leave', socket.id);
-      delete users[socket.id];
-      delete members[socket.id];
-    }
+    console.log('a user disconnected');
   });
 });
 
